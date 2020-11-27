814a83c6812d83da8a582341ecb5611f1d340c77
aldiaaan <aldian.asmr@gmail.com>
2020-11-27 08:33:51 +0700

    add useLocalStorage hooks

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,33 @@
+import { useState, useCallback } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const destroy = useCallback(() => window.localStorage.removeItem(key), [key]);
+
+  const setValue = useCallback(
+    (value) => {
+      try {
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value;
+        setStoredValue(valueToStore);
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [key]
+  );
+
+  return [storedValue, setValue, destroy];
+};
+
+export default useLocalStorage;
